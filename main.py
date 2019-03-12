9d0b38a3bdaa7dea3435faef3494c1007678e842
Jonas Laksen <jonaslaksen@live.com>
2019-03-12 15:00:32 +0100

    Add previous values to datafile

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ import pandas as pd
 
 dfs = [data_from_stock('GOOG'), data_from_stock('AAPL'), data_from_stock('DIS'), data_from_stock('TSLA'), data_from_stock('TWTR')]
 
+def add_prev_feature(df, feature, n):
+    for i in range(n):
+        df[f'prev_{feature}_{i}'] = df[feature].shift(-(i+1))
+
+for df in dfs:
+    for feature in ['price','volume','trendscore','positive','negative','neutral']:
+        add_prev_feature(df, feature, 2)
+
+#print(dfs[0][:5])
 pd.concat(dfs).to_csv('dataset.csv')
-
-
-
