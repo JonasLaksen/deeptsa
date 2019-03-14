517d1f27d40d049a01916b9fe9b07344b0984c1b
Jonas Laksen <jonaslaksen@live.com>
2019-03-14 15:26:28 +0100

    Add next price

--- main.py
+++ main.py
@@ -8,8 +8,12 @@ def add_prev_feature(df, feature, n):
         df[f'prev_{feature}_{i}'] = df[feature].shift(-(i+1))
 
 for df in dfs:
+    df['next_price'] = df['price'].shift(1)
     for feature in ['price','volume','trendscore','positive','negative','neutral']:
         add_prev_feature(df, feature, 2)
 
+dfs = map(lambda x: x[1:-2], dfs)
+
 #print(dfs[0][:5])
 pd.concat(dfs).to_csv('dataset.csv')
+
