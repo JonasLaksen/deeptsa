dcf233bce261730c4858567d591a41f03c94a46e
pthoang <pthoang94@gmail.com>
2019-03-28 16:38:21 +0100

    add specialized network

--- lstm.py
+++ lstm.py
@@ -21,7 +21,67 @@ def build_network(n_features, layer_sizes, stateful, batch_size, loss='mse'):
     model.add(keras.layers.Dense(1, activation='linear'))
 
     model.compile(
-        optimizer=tf.keras.optimizers.Adam(0.001),
+        optimizer=keras.optimizers.Adam(0.001),
+        loss=keras.losses.MAE,
+        metrics=[keras.metrics.MAPE]
+    )
+
+    return model
+
+def build_spes_network(n_features, num_stocks, layer_sizes, stateful, batch_size, loss='mse'):
+    encoder_inputs = keras.layers.Input(shape=(1, 1))
+
+    decoder_inputs = keras.layers.Input(batch_shape=(batch_size, None, n_features))
+
+    encoder_embed_h = keras.layers.Embedding(num_stocks + 1, layer_sizes[0])
+    encoder_reshape = keras.layers.Reshape((1, layer_sizes[0]), input_shape=(None, 1, 1, layer_sizes[0]))
+    # encoder_embed_lambda = keras.layers.Lambda(lambda input: tf.reshape(input, [1,self.gen_dims[1]]))
+    encoder_embed_lambda = keras.layers.Lambda(lambda input: tf.reshape(input, [batch_size, layer_sizes[0]]))
+    state_h = encoder_embed_h(encoder_inputs)
+    state_h = encoder_embed_lambda(state_h)
+
+    encoder_embed_c = keras.layers.Embedding(num_stocks + 1, layer_sizes[0])
+    state_c = encoder_embed_c(encoder_inputs)
+    state_c = encoder_embed_lambda(state_c)
+
+    init_state = [state_h, state_c]
+
+    decoder_input_layer = keras.layers.Masking(mask_value=0.,
+                                               batch_input_shape=(batch_size, None, n_features))
+    # decoder_input.set_weights(self.generalist.layers[0].get_weights())
+    decoder_lstm = keras.layers.LSTM(layer_sizes[0], return_sequences=True,
+                                     return_state=True,
+                                     stateful=stateful)
+    decoder_dropout = keras.layers.Dropout(0.2)
+
+    decoder_output = keras.layers.Dense(1, activation='linear')
+
+    # decoder_output.set_weights(self.generalist.layers[-1].get_weights())
+
+    # encoder = keras.layers.LSTM(self.gen_dims[1], return_sequences=True, return_state=True)
+
+    # encoder_outputs, state_h, state_c = encoder(encoder_inputs)
+
+    output = decoder_input_layer(decoder_inputs)
+    # print(self.num_tags)
+
+    output, _, _ = decoder_lstm(output, initial_state=init_state)
+    output = decoder_dropout(output)
+
+    output = decoder_output(output)
+
+    model = keras.Model([decoder_inputs, encoder_inputs], output)
+
+    # for i, layer in enumerate(model.layers):
+    #     print(i, layer)
+    #
+    # copy_weight_indexes = [4, 6, 7, 8]
+    #
+    # for i, layer_i in enumerate(copy_weight_indexes):
+    #     model.layers[layer_i].set_weights(self.generalist.layers[i].get_weights())
+
+    model.compile(
+        optimizer=keras.optimizers.Adam(0.001),
         loss=keras.losses.MAE,
         metrics=[keras.metrics.MAPE]
     )
@@ -74,7 +134,7 @@ if __name__ == '__main__':
     y_no_pad, y, y_test = group_by_stock(y, 1)
 
 
-    model.fit(X, y, batch_size=batch_size, epochs=250, shuffle=False)
+    model.fit(X, y, batch_size=batch_size, epochs=1, shuffle=False)
 
     print(model.summary())
 
@@ -109,11 +169,44 @@ if __name__ == '__main__':
     # y_test = y[stock_id].reshape(-1)
 
 
+    # pd.DataFrame({'Predicted': result}).plot(label='Predicted', c='b')
+    # pd.DataFrame({'Actual': y_test})['Actual'].plot(label='Actual', c='r', linestyle='--')
+    #
+    # pd.DataFrame({'Predicted': result_train}).plot(label='Predicted', c='g')
+    # pd.DataFrame({'Actual': y_train})['Actual'].plot(label='Actual', c='y', linestyle='--')
+
+    spes_model = build_spes_network(n_features=n_features, num_stocks=len(y), layer_sizes=layer_sizes,
+                                    stateful=False, batch_size=batch_size)
+
+    X_stock_list = np.array([[[i]] for i in range(len(X))])
+    spes_model.fit([X, X_stock_list], y, batch_size=batch_size, epochs=350, shuffle=False)
+
+    spes_pred_model = build_spes_network(n_features=n_features, num_stocks=len(y), layer_sizes=layer_sizes,
+                                    stateful=True, batch_size=1)
+
+    spes_pred_model.set_weights(spes_model.get_weights())
+
+    result_train = spes_pred_model.predict([X_no_pad[stock_id].reshape(1, X_no_pad[stock_id].shape[0],
+                                                                 X_no_pad[stock_id].shape[1]),
+                                       np.array([[[stock_id]]])])
+
+    result_train = min_max_scaler_y.inverse_transform(result_train[0]).reshape(-1)
+    y_train = min_max_scaler_y.inverse_transform(y_no_pad[stock_id]).reshape(-1)
+
+    result = spes_pred_model.predict([X_test[stock_id].reshape(1, X_test[stock_id].shape[0],
+                                                                 X_test[stock_id].shape[1]),
+                                       np.array([[[stock_id]]])])
+
+    result = min_max_scaler_y.inverse_transform(result[0]).reshape(-1)
+    # y_test = min_max_scaler_y.inverse_transform(y_test[stock_id]).reshape(-1)
+
     pd.DataFrame({'Predicted': result}).plot(label='Predicted', c='b')
     pd.DataFrame({'Actual': y_test})['Actual'].plot(label='Actual', c='r', linestyle='--')
-
     pd.DataFrame({'Predicted': result_train}).plot(label='Predicted', c='g')
     pd.DataFrame({'Actual': y_train})['Actual'].plot(label='Actual', c='y', linestyle='--')
+
+    print(spes_model.summary())
+
 
 
     plt.show()
