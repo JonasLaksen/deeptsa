2f3faec387768eba7dabac71a119c7bcb16372ad
Jonas Laksen <jonaslaksen@live.com>
2019-04-28 00:22:10 +0200

    Divide network into subnetworks

--- lstm.py
+++ lstm.py
@@ -1,49 +1,29 @@
-import tensorflow as tf
-from tensorflow import keras
-import pandas as pd
 import copy
-from sklearn import preprocessing
+import keras
+import keras.layers
 import matplotlib.pyplot as plt
-from constants import stock_list
 import numpy as np
-
-def build_network(n_features, layer_sizes, stateful, batch_size, loss='mse'):
-
-    # inputs = keras.layers.Input(batch_shape=(batch_size, None, n_features))
-    #
-    # input_masking = keras.layers.Masking(mask_value=0.,
-    #                                            batch_input_shape=(batch_size, None, n_features))
-    #
-    # lstms = {}
-    # dropouts = {}
-    # for i, size in enumerate(layer_sizes):
-    #     lstms[i] = keras.layers.LSTM(size, return_sequences=True,
-    #                                          return_state=True,
-    #                                          stateful=stateful)
-    #     dropouts[i] = keras.layers.Dropout(0.2)
-    #
-    # output_dense = keras.layers.Dense(1, activation='linear')
-    #
-    # output = input_masking(inputs)
-    #
-    # for i in range(len(layer_sizes)):
-    #     if i == 0:
-    #         output, state_h, state_c = lstms[i](output)
-    #     else:
-    #         output, state_h, state_c = lstms[i](output, initial_state=init_state)
-    #     output = dropouts[i](output)
-    #     init_state = [state_h, state_c]
-    #
-    # output = output_dense(output)
-    #
-    # model = keras.Model(inputs, output)
+import pandas as pd
+from keras import Input
+from keras.callbacks import ModelCheckpoint
+from keras.engine.saving import load_model
+from keras.layers import LSTM, Dense, Reshape, Embedding, Masking
+from sklearn import preprocessing
 
 
+def expand(x): np.expand_dims(x, axis=0)
+
+
+def plot(result, y):
+    pd.DataFrame({'Predicted': result}).plot(label='Predicted', c='b')
+    pd.DataFrame({'Actual': y})['Actual'].plot(label='Actual', c='r', linestyle='--')
+
+
+def build_gen_network(n_features, layer_sizes, stateful, batch_size):
     model = keras.Sequential()
     model.add(keras.layers.Masking(mask_value=0.0, batch_input_shape=(batch_size, None, n_features)))
     for i, layer_size in enumerate(layer_sizes):
-        model.add(keras.layers.LSTM(layer_size, return_sequences=True, stateful=stateful))
-        model.add(keras.layers.Dropout(0.2))
+        model.add(keras.layers.LSTM(layer_size, return_sequences=True, stateful=stateful, dropout=.2))
 
     model.add(keras.layers.Dense(1, activation='linear'))
 
@@ -55,93 +35,58 @@ def build_network(n_features, layer_sizes, stateful, batch_size, loss='mse'):
 
     return model
 
-def build_spes_network(n_features, num_stocks, layer_sizes, stateful, batch_size, gen_model, spes_model, loss='mse'):
+
+def build_encoder(num_stocks, layer_sizes):
     encoder_inputs = keras.layers.Input(shape=(1, 1))
 
-    decoder_inputs = keras.layers.Input(batch_shape=(batch_size, None, n_features))
+    encoder_reshape = Reshape((layer_sizes[0],))
+    init_h = Embedding(num_stocks + 1, layer_sizes[0])(encoder_inputs)
+    init_h = encoder_reshape(init_h)
 
-    encoder_embed_h = keras.layers.Embedding(num_stocks + 1, layer_sizes[0])
-    encoder_reshape = keras.layers.Reshape((1, layer_sizes[0]), input_shape=(None, 1, 1, layer_sizes[0]))
-    # encoder_embed_lambda = keras.layers.Lambda(lambda input: tf.reshape(input, [1,self.gen_dims[1]]))
-    encoder_embed_lambda = keras.layers.Lambda(lambda input: tf.reshape(input, [batch_size, layer_sizes[0]]))
-    state_h = encoder_embed_h(encoder_inputs)
-    state_h = encoder_embed_lambda(state_h)
+    init_c = Embedding(num_stocks + 1, layer_sizes[0])(encoder_inputs)
+    init_c = encoder_reshape(init_c)
 
-    encoder_embed_c = keras.layers.Embedding(num_stocks + 1, layer_sizes[0])
-    state_c = encoder_embed_c(encoder_inputs)
-    state_c = encoder_embed_lambda(state_c)
-
-    init_state = [state_h, state_c]
-
-    decoder_input_layer = keras.layers.Masking(mask_value=0.,
-                                               batch_input_shape=(batch_size, None, n_features))
-    # decoder_input.set_weights(self.generalist.layers[0].get_weights())
-
-    decoder_lstms = {}
-    decoder_dropouts = {}
-    for i, size in enumerate(layer_sizes):
-        decoder_lstms[i] = keras.layers.LSTM(size, return_sequences=True,
-                                             return_state=True,
-                                             stateful=stateful)
-        decoder_dropouts[i] = keras.layers.Dropout(0.2)
-
-    decoder_dense = keras.layers.Dense(1, activation='linear')
-
-    # decoder_dense.set_weights(self.generalist.layers[-1].get_weights())
-
-    # encoder = keras.layers.LSTM(self.gen_dims[1], return_sequences=True, return_state=True)
-
-    # encoder_outputs, state_h, state_c = encoder(encoder_inputs)
-
-    output = decoder_input_layer(decoder_inputs)
-    # print(self.num_tags)
+    return keras.Model(encoder_inputs, [init_h, init_c])
 
 
-    for i in range(len(layer_sizes)):
-        next_state = [state_h, state_c]
-        if i == 0:
-            output, state_h, state_c = decoder_lstms[i](output, initial_state=init_state)
-        else:
-            output, state_h, state_c = decoder_lstms[i](output, initial_state=next_state)
-        output = decoder_dropouts[i](output)
+def build_decoder(n_features, layer_sizes, stateful, batch_size):
+    X = Input(batch_shape=(batch_size, None, n_features))
+    h_input, c_input = Input(shape=(layer_sizes[0],)), Input(shape=(layer_sizes[0],))
 
-    output = decoder_dense(output)
+    masked = Masking(mask_value=0., batch_input_shape=(batch_size, None, n_features))
+    state_h, state_c = h_input, c_input
+    output = masked(X)
 
-    model = keras.Model([decoder_inputs, encoder_inputs], output)
+    for size in layer_sizes:
+        lstm = LSTM(size, return_sequences=True, return_state=True, stateful=stateful, dropout=.2)
+        output, state_h, state_c = lstm(output, initial_state=[state_h, state_c])
 
-    model.compile(
-        optimizer=keras.optimizers.Adam(0.0001),
-        loss=keras.losses.MAE,
-        metrics=[keras.metrics.MAPE]
-    )
+    next_price = Dense(1, activation='linear')(output)
 
-    copy_weights_layers = [4, 6, 7, 8, 9, 10, 11, 12]
+    model = keras.Model([X, h_input, c_input], [next_price, state_h, state_c])
+    return model
 
+
+def build_spec_network(n_features, num_stocks, layer_sizes, stateful, batch_size, gen_model, spec_model):
+    stock_id = Input(shape=(1, 1))
+    encoder = build_encoder(num_stocks, layer_sizes)
+    state_h, state_c = encoder(stock_id)
+
+    X = Input(batch_shape=(batch_size, None, n_features))
+    decoder = build_decoder(n_features, layer_sizes, stateful, batch_size)
+    next_price, state_h, state_c = decoder([X, state_h, state_c])
+
+    model = keras.Model([X, stock_id], next_price)
+
+    copy_weights_layers = [4, 5, 6, 7]
     for i, layer_i in enumerate(copy_weights_layers):
-        model.layers[layer_i].set_weights(gen_model.layers[i].get_weights())
+        decoder.layers[layer_i].set_weights(gen_model.layers[i + 1].get_weights())
 
-    if(spes_model):
-        model.set_weights(spes_model.get_weights())
+    if spec_model:
+        model.set_weights(spec_model.get_weights())
 
-    encoder_model = keras.Model(encoder_inputs, init_state)
+    return model, encoder, decoder
 
-    decoder_state_inputs = [keras.layers.Input(shape=(layer_sizes[0],)),
-                            keras.layers.Input(shape=(layer_sizes[0],))]
-    decoder_outputs = decoder_input_layer(decoder_inputs)
-
-    for i in range(len(layer_sizes)):
-        next_state = [state_h, state_c]
-        if i == 0:
-            decoder_outputs, state_h, state_c = decoder_lstms[i](decoder_outputs, initial_state=decoder_state_inputs)
-        else:
-            decoder_outputs, state_h, state_c = decoder_lstms[i](decoder_outputs, initial_state=next_state)
-        decoder_outputs = decoder_dropouts[i](decoder_outputs)
-    decoder_states = [state_h, state_c]
-    decoder_outputs = decoder_dense(decoder_outputs)
-
-    decoder_model = keras.Model([decoder_inputs] + decoder_state_inputs, [decoder_outputs] + decoder_states)
-
-    return model, encoder_model, decoder_model
 
 def group_by_stock(data, n_features):
     group_by_dict = {}
@@ -162,11 +107,10 @@ def group_by_stock(data, n_features):
 
     return np.array(data), np.array(data_padded), np.array(test_data)
 
-def main(mode='gen'):
+
+def main(mode='gen', both=False, gen_epochs=0, spec_epochs=0):
     data = pd.read_csv('dataset.csv', index_col=0)
     data = data.dropna()
-
-    # data = data[data['stock'] == 'AAPL']
 
     # feature_list = ['positive', 'negative', 'neutral', 'open', 'high', 'low', 'volume', 'price']
     feature_list = ['open', 'high', 'low', 'volume', 'price']
@@ -174,104 +118,76 @@ def main(mode='gen'):
     n_features = len(feature_list)
     layer_sizes = [41, 41, 41]
     batch_size = 17
-    model = build_network(n_features=n_features, layer_sizes=layer_sizes, batch_size=batch_size, stateful=False)
 
-    min_max_scaler_X = preprocessing.MinMaxScaler()
-    min_max_scaler_y = preprocessing.MinMaxScaler()
-    X = min_max_scaler_X.fit_transform(data[feature_list].values)
-    y = min_max_scaler_y.fit_transform(data['next_price'].values.reshape(-1, 1))
-    # y = data['next_price'].values.reshape(-1, 1)
+    scaler_X = preprocessing.MinMaxScaler()
+    scaler_y = preprocessing.MinMaxScaler()
+    X = scaler_X.fit_transform(data[feature_list].values)
+    y = scaler_y.fit_transform(data['next_price'].values.reshape(-1, 1))
     X = np.append(data['stock'].values.reshape(-1, 1), X, axis=1)
     y = np.append(data['stock'].values.reshape(-1, 1), y, axis=1)
 
-
     X_no_pad, X, X_test = group_by_stock(X, n_features)
     y_no_pad, y, y_test = group_by_stock(y, 1)
-
     stock_id = 12
-    if mode == 'gen':
 
+    X_train, X_test = expand(X_no_pad[stock_id]), expand(X_test[stock_id])
+    y_train = scaler_y.inverse_transform(y_no_pad[stock_id]).reshape(-1)
+    y_test = scaler_y.inverse_transform(y_test[stock_id]).reshape(-1)
 
-        model.fit(X, y, batch_size=batch_size, epochs=300, shuffle=False)
+    try:
+        gen_model = load_model('model/gen.h5')
+        print('Loaded generalised model')
+    except OSError:
+        print('Couldn\'t load generalised model')
+        gen_model = None
 
+    if mode == 'gen' or both:
+        if not gen_model:
+            print('Building new generalised model')
+            gen_model = build_gen_network(n_features=n_features, layer_sizes=layer_sizes, batch_size=batch_size,
+                                          stateful=False)
 
-        pred_model = build_network(n_features=n_features, layer_sizes=layer_sizes, batch_size=1,stateful=True)
+        # Save model every epoch
+        gen_model.fit(X, y, batch_size=batch_size, epochs=gen_epochs, shuffle=False,
+                      callbacks=[ModelCheckpoint('model/gen.h5')])
 
+        gen_pred_model = build_gen_network(n_features=n_features, layer_sizes=layer_sizes, batch_size=1, stateful=True)
+        gen_pred_model.set_weights(gen_model.get_weights())
 
-        pred_model.set_weights(model.get_weights())
+        result_train = gen_pred_model.predict(X_train)
+        result_train = scaler_y.inverse_transform(result_train[0]).reshape(-1)
 
+        result_test = gen_pred_model.predict(X_test)
+        result_test = scaler_y.inverse_transform(result_test[0]).reshape(-1)
 
-        X_reshaped = X[stock_id].reshape(1, X[stock_id].shape[0], X[stock_id].shape[1])
-        y_reshaped = y[stock_id].reshape(1, y[stock_id].shape[0], y[stock_id].shape[1])
+        [plot(*x) for x in [(result_train, y_train), (result_test, y_test)]]
 
+    if mode == 'spec' or both:
+        spec_model, encoder, decoder = build_spec_network(n_features=n_features, num_stocks=len(y),
+                                                          layer_sizes=layer_sizes, stateful=False, gen_model=gen_model,
+                                                          spec_model=None, batch_size=batch_size)
 
-        result_train = pred_model.predict(X_no_pad[stock_id].reshape(1, X_no_pad[stock_id].shape[0],
-                                                                     X_no_pad[stock_id].shape[1]))
-        result_train = result = min_max_scaler_y.inverse_transform(result_train[0]).reshape(-1)
-        y_train = min_max_scaler_y.inverse_transform(y_no_pad[stock_id]).reshape(-1)
+        spec_model.compile(
+            optimizer=keras.optimizers.Adam(0.001),
+            loss=keras.losses.MAE,
+            metrics=[keras.metrics.MAPE]
+        )
 
+        X_stock_list = np.arange(len(X)).reshape((len(X), 1, 1))
+        spec_model.fit([X, X_stock_list], y, batch_size=batch_size, epochs=spec_epochs, shuffle=False)
 
+        spec_pred_model, _, _ = build_spec_network(n_features=n_features, num_stocks=len(y), layer_sizes=layer_sizes,
+                                                   stateful=True, gen_model=gen_model, spec_model=spec_model,
+                                                   batch_size=1)
 
-        result = pred_model.predict(X_test[stock_id].reshape(1, X_test[stock_id].shape[0], X_test[stock_id].shape[1]))
-        # result = pred_model.predict(X[stock_id].reshape(1, X[stock_id].shape[0], X[stock_id].shape[1]))
-        # result = pred_model.predict(X[stock_id].reshape(1, X[stock_id].shape[0], X[stock_id].shape[1]))[0].reshape(-1)
+        result_train = spec_pred_model.predict([X_train, np.array([[[stock_id]]])])
+        result_train = scaler_y.inverse_transform(result_train[0]).reshape(-1)
 
-        result = min_max_scaler_y.inverse_transform(result[0]).reshape(-1)
-        y_test = min_max_scaler_y.inverse_transform(y_test[stock_id]).reshape(-1)
-        # y_test = min_max_scaler_y.inverse_transform(y[stock_id]).reshape(-1)
-        # y_test = y[stock_id].reshape(-1)
+        result_test = spec_pred_model.predict([X_test, np.array([[[stock_id]]])])
+        result_test = scaler_y.inverse_transform(result_test[0]).reshape(-1)
 
-
-        pd.DataFrame({'Predicted': result}).plot(label='Predicted', c='b')
-        pd.DataFrame({'Actual': y_test})['Actual'].plot(label='Actual', c='r', linestyle='--')
-
-        pd.DataFrame({'Predicted': result_train}).plot(label='Predicted', c='g')
-        pd.DataFrame({'Actual': y_train})['Actual'].plot(label='Actual', c='y', linestyle='--')
-
-        model.save_weights('model/gen_weights.h5')
-        model.save('model/gen.h5')
-
-
-    else:
-
-        gen_model = keras.models.load_model('model/gen.h5')
-
-        spes_model, _, _ = build_spes_network(n_features=n_features, num_stocks=len(y), layer_sizes=layer_sizes,
-                                        stateful=False, gen_model=gen_model, spes_model=None, batch_size=batch_size)
-
-
-        X_stock_list = np.array([[[i]] for i in range(len(X))])
-        spes_model.fit([X, X_stock_list], y, batch_size=batch_size, epochs=1, shuffle=False)
-
-        spes_pred_model, encoder_model, decoder_model = build_spes_network(n_features=n_features, num_stocks=len(y), layer_sizes=layer_sizes,
-                                             stateful=True, gen_model=gen_model, spes_model=spes_model, batch_size=1)
-
-
-        return
-        result_train, = spes_pred_model.predict([X_no_pad[stock_id].reshape(1, X_no_pad[stock_id].shape[0],
-                                                                           X_no_pad[stock_id].shape[1]),
-                                                np.array([[[stock_id]]])])
-
-        result_train = min_max_scaler_y.inverse_transform(result_train[0]).reshape(-1)
-        y_train = min_max_scaler_y.inverse_transform(y_no_pad[stock_id]).reshape(-1)
-
-
-        result, _, _ = spes_pred_model.predict([X_test[stock_id].reshape(1, X_test[stock_id].shape[0],
-                                                                   X_test[stock_id].shape[1]),
-                                          np.array([[[stock_id]]])])
-
-        result = min_max_scaler_y.inverse_transform(result[0]).reshape(-1)
-        y_test = min_max_scaler_y.inverse_transform(y_test[stock_id]).reshape(-1)
-
-        pd.DataFrame({'Predicted': result}).plot(label='Predicted', c='b')
-        pd.DataFrame({'Actual': y_test})['Actual'].plot(label='Actual', c='r', linestyle='--')
-        pd.DataFrame({'Predicted': result_train}).plot(label='Predicted', c='g')
-        pd.DataFrame({'Actual': y_train})['Actual'].plot(label='Actual', c='y', linestyle='--')
-
-        print(spes_model.summary())
-
-
-
+        [plot(*x) for x in [(result_train, y_train), (result_test, y_test)]]
     plt.show()
 
-main('spes')
+
+main('spec', False, gen_epochs=0, spec_epochs=10)
