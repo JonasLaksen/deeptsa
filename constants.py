77b8383b03ed082aa4eb21e01bd092bec78321fb
pthoang <pthoang94@gmail.com>
2019-05-13 18:52:16 +0200

    add more data, add dataset_v2.csv

--- constants.py
+++ constants.py
@@ -1,2 +1,4 @@
 stock_list = ['AAPL', 'AMD', 'AMZN', 'BIDU', 'DIS', 'FB', 'GOOG', 'HD', 'INTC', 'KO', 'NFLX', 'NVDA', 'PFE', 'QCOM',
-              'TSLA', 'TWTR', 'WMT']
+              'TSLA', 'TWTR', 'WMT', 'CSCO', 'CTSH', 'SBUX', 'EBAY', 'MDLZ', 'ATVI', 'EA', 'WDC', 'TXN', 'PEP', 'EXPE',
+              'ADBE', 'COST', 'HAS', 'SYMC', 'MSFT', 'SIRI', 'MU', 'GILD', 'MRVL', 'ODP', 'MAT', 'GRMN', 'PHG', 'BA',
+              'IBM', 'MCD', 'GS']
