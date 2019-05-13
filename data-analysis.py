0dde1534118a46079836330685840dbc3b435fc4
pthoang <pthoang94@gmail.com>
2019-05-13 13:26:34 +0200

    add acf pacf plotting

--- data-analysis.py
+++ data-analysis.py
@@ -1,14 +1,24 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+import constants
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 
 def plot_correlation(data, x_name, y_name):
     sns.jointplot(x=x_name, y=y_name, data=data)
 
 
 if __name__ == '__main__':
-    data = pd.read_csv('dataset.csv')
-    data = data[data['stock'] == 'FB']
-    plot_correlation(data, 'negative', 'next_price')
-    plt.show()
+    all_data = pd.read_csv('dataset.csv')
+    for stock in constants.stock_list:
+
+
+        data = all_data[all_data['stock'] == stock]
+        # plot_correlation(data, 'negative', 'next_price')
+
+        acf = plot_acf(data[['price']].values, lags=200, title=stock + ' Autocorrelation')
+        pacf = plot_pacf(data[['price']].values, lags=85, title=stock + ' Partial Autocorrelation')
+        acf.savefig('./figures/' + stock + '_acf.png')
+        plt.close(acf)
+        pacf.savefig('./figures/' + stock + '_pacf.png')
+        plt.close(pacf)
