519c6a201cb40200f4eb1e2cfb5902e8c04bad7b
Jonas Laksen <jonaslaksen@live.com>
2019-05-19 18:50:28 +0200

    Save plots and models to file

--- lstm.py
+++ lstm.py
@@ -1,3 +1,8 @@
+import csv
+import json
+from os import listdir
+from random import sample, randint
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -6,16 +11,16 @@ from keras.losses import MAE
 from keras.optimizers import Adam
 from sklearn.preprocessing import MinMaxScaler
 
+from models import bidir_lstm_seq
 from models.spec_network import SpecializedNetwork
 from models.stacked_lstm import StackedLSTM
-from utils import get_feature_list_lags, group_by_stock, plot
+from utils import get_feature_list_lags, group_by_stock, write_to_csv, from_args_to_filename, \
+    from_filename_to_args, plot_data
 
 
-def main(gen_epochs=0, spec_epochs=0, load_gen=True, load_spec=False, model_generator=StackedLSTM, layer_sizes=[41],
-         copy_weights_from_gen_to_spec=False, feature_list=[]):
-    data = pd.read_csv('dataset.csv', index_col=0)
+def load_data(feature_list):
+    data = pd.read_csv('dataset_v2.csv', index_col=0)
     data = data.dropna()
-    n_features = len(feature_list)
     scaler_X = MinMaxScaler()
     scaler_y = MinMaxScaler()
     X = scaler_X.fit_transform(data[feature_list].values)
@@ -25,7 +30,16 @@ def main(gen_epochs=0, spec_epochs=0, load_gen=True, load_spec=False, model_gene
 
     X_train, X_val, X_test = group_by_stock(X)
     y_train, y_val, y_test = group_by_stock(y)
+    return (X_train, X_val, X_test), (y_train, y_val, y_test), scaler_y
 
+
+def main(gen_epochs=0, spec_epochs=0, load_gen=True, load_spec=False, model_generator=StackedLSTM, layer_sizes=[41],
+         copy_weights_from_gen_to_spec=False, feature_list=[], learning_rate=.001, dropout=.2, filename='test', **_):
+    (X_train, X_val, X_test), \
+    (y_train, y_val, y_test), \
+    scaler_y = load_data(feature_list)
+
+    n_features = X_train.shape[2]
     batch_size = X_train.shape[0]
     is_bidir = model_generator is not StackedLSTM
     zero_states = [np.zeros((batch_size, layer_sizes[0]))] * len(layer_sizes) * 2 * (2 if is_bidir else 1)
@@ -38,29 +52,26 @@ def main(gen_epochs=0, spec_epochs=0, load_gen=True, load_spec=False, model_gene
         print('Loaded generalised model')
 
     # Create the general model
-    gen_model.compile(optimizer=Adam(0.001), loss=MAE)
-    history = gen_model.fit([X_train] + zero_states, y_train, validation_data=([X_val] + zero_states, y_val), epochs=gen_epochs,
-                  shuffle=False,
-                  callbacks=[ModelCheckpoint('weights/gen.h5', period=10, save_weights_only=True), ])
+    gen_model.compile(optimizer=Adam(learning_rate), loss=MAE)
+    history = gen_model.fit([X_train] + zero_states, y_train, validation_data=([X_val] + zero_states, y_val),
+                            epochs=gen_epochs,
+                            shuffle=False,
+                            callbacks=[ModelCheckpoint('weights/gen.h5', period=10, save_weights_only=True), ])
 
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title('Model loss')
-    plt.ylabel('Loss')
-    plt.xlabel('Epoch')
-    plt.legend(['Train', 'Test'], loc='upper left')
+    write_to_csv(f'plot_data/gen/loss/{filename}.csv', history.history)
 
     gen_pred_model = model_generator(n_features=n_features, layer_sizes=layer_sizes, batch_size=batch_size,
-                                     return_states=True)
+                                     return_states=True, dropout=dropout)
     gen_pred_model.set_weights(gen_model.get_weights())
 
     # Create the context model, set the decoder = the gen model
-    decoder = model_generator(n_features=n_features, layer_sizes=layer_sizes, batch_size=batch_size, return_states=True)
+    decoder = model_generator(n_features=n_features, layer_sizes=layer_sizes, batch_size=batch_size, return_states=True,
+                              dropout=dropout)
     if copy_weights_from_gen_to_spec:
         decoder.set_weights(gen_model.get_weights())
-    spec_model = SpecializedNetwork(n_features=n_features, num_stocks=len(y), layer_sizes=layer_sizes,
+    spec_model = SpecializedNetwork(n_features=n_features, num_stocks=len(X_train), layer_sizes=layer_sizes,
                                     decoder=decoder, is_bidir=is_bidir)
-    spec_model.compile(optimizer=Adam(0.001), loss=MAE)
+    spec_model.compile(optimizer=Adam(learning_rate), loss=MAE)
     if load_spec:
         spec_model.load_weights('weights/spec.h5')
         print('Loaded specialised model')
@@ -68,11 +79,12 @@ def main(gen_epochs=0, spec_epochs=0, load_gen=True, load_spec=False, model_gene
     spec_model.fit([X_train] + stock_list, y_train, validation_data=([X_val] + stock_list, y_val),
                    batch_size=batch_size, epochs=spec_epochs, shuffle=False,
                    callbacks=[ModelCheckpoint('weights/spec.h5', period=1, save_weights_only=True)])
-    spec_pred_model = SpecializedNetwork(n_features=n_features, num_stocks=len(y), layer_sizes=layer_sizes,
+    write_to_csv(f'plot_data/spec/loss/{filename}.csv', history.history)
+    spec_pred_model = SpecializedNetwork(n_features=n_features, num_stocks=len(X_train), layer_sizes=layer_sizes,
                                          return_states=True, decoder=spec_model.decoder, is_bidir=is_bidir)
     spec_pred_model.set_weights(spec_model.get_weights())
 
-    # The place for plotting stuff
+    # The place for saving stuff for plotting
     # Only plot if the epoch > 0
     for model in ([gen_pred_model] if gen_epochs > 0 else []) + ([spec_pred_model] if spec_epochs > 0 else []):
         has_context = isinstance(model, SpecializedNetwork)
@@ -92,9 +104,12 @@ def main(gen_epochs=0, spec_epochs=0, load_gen=True, load_spec=False, model_gene
             lambda x: scaler_y.inverse_transform(x[stock_id]).reshape(-1),
             [result_train, result_val, result_test, y_train, y_val, y_test])
 
-        [plot(*x) for x in [(f'training {"spec" if has_context else "gen"}', result_train, y_train_inv),
-                            (f'validation {"spec" if has_context else "gen"}', result_val, y_val_inv)]]
-    plt.show()
+        training = {f'training {"spec" if has_context else "gen"}': result_train.tolist(), 'y': y_train_inv.tolist()}
+        validation = {f'validation {"spec" if has_context else "gen"}': result_val.tolist(), 'y': y_val_inv.tolist()}
+        write_to_csv(f'plot_data/{"spec" if has_context else "gen"}/training/{filename}', training)
+        write_to_csv(f'plot_data/{"spec" if has_context else "gen"}/validation/{filename}', validation)
+    gen_pred_model.save(f'saved_models/gen/{filename}')
+    spec_pred_model.save(f'saved_models/spec/{filename}')
 
 
 # feature_list = ['positive', 'negative', 'neutral', 'open', 'high', 'low', 'volume', 'price']
@@ -102,14 +117,45 @@ feature_list = ['volume', 'price']
 feature_list = get_feature_list_lags(feature_list, lags=2)
 feature_list = feature_list + ['open', 'high', 'low']
 
-main(
-    copy_weights_from_gen_to_spec=False,
-    feature_list=feature_list,
-    gen_epochs=200,
-    spec_epochs=0,
-    layer_sizes=[41] * 1,
-    load_gen=False,
-    load_spec=False,
-    # model_generator=bidir_lstm_seq.build_model,
-    model_generator=StackedLSTM,
-)
+arguments = {
+    'copy_weights_from_gen_to_spec': False,
+    'feature_list': feature_list,
+    'dropout': .2,
+    'gen_epochs': 2,
+    'spec_epochs': 0,
+    'layer_sizes': [41] * 1,
+    'learning_rate': .001,
+    'load_gen': False,
+    'load_spec': False,
+    'model': 'stacked',
+    # 'model': 'bidir',
+}
+
+
+def random_arguments():
+    args = {
+        'copy_weights_from_gen_to_spec': False,
+        'feature_list': list(set(sample(feature_list, randint(1, len(feature_list))) + ['price'])),
+        'dropout': .1 * randint(1, 8),
+        'gen_epochs': 1000,
+        'spec_epochs': 1000,
+        'layer_sizes': [2 ** randint(1, 8)] * 1,
+        'learning_rate': 10 ** (-randint(1, 4)),
+        'load_gen': False,
+        'load_spec': False,
+        'model': 'stacked',
+        # 'model': 'bidir',
+    }
+    return args, from_args_to_filename(args)
+
+
+# Generate random arguments to test an arbitrary amount of new models
+for i in range(100):
+    args, filename = random_arguments()
+    main(**args,
+         model_generator=StackedLSTM if arguments['model'] == 'stacked' else bidir_lstm_seq.build_model,
+         filename=filename)
+
+
+all_files = list(map(lambda x: (json.loads(from_filename_to_args(x)), x), listdir('plot_data/gen/training')))
+[plot_data(i, f'plot_data/gen/training/{x}') for i, (_, x) in enumerate(all_files)]
--- utils.py
+++ utils.py
@@ -1,7 +1,13 @@
 import copy
+import csv
+import json
+from base64 import b64encode, b64decode
+from zlib import compress, decompress
+import matplotlib.pyplot as plt
 
 import numpy as np
 import pandas as pd
+from matplotlib import pyplot
 from sklearn.metrics import mean_absolute_error, mean_squared_error, accuracy_score
 
 
@@ -14,13 +20,18 @@ def mean_absolute_percentage_error(y_true, y_pred):
 
 
 def plot(title, result, y):
-    mape = mean_absolute_percentage_error(y, result)
-    mae = mean_absolute_error(y, result)
-    mse = mean_squared_error(y, result)
-    accuracy_direction = direction_eval(result, y)
-    print(mape, mae, mse, accuracy_direction)
-    pd.DataFrame({'Predicted': result}).plot(label='Predicted', c='b', title=title)
-    pd.DataFrame({'Actual': y})['Actual'].plot(label='Actual', c='r', linestyle='--')
+    # result = np.asarray(result)
+    # y = np.asarray(y)
+    # mape = mean_absolute_percentage_error(y, result)
+    # mae = mean_absolute_error(y, result)
+    # mse = mean_squared_error(y, result)
+    # accuracy_direction = direction_eval(result, y)
+    # print(mape, mae, mse, accuracy_direction)
+    # pd.DataFrame({'Predicted': result}).plot(label='Predicted', c='b', title=title)
+    # pd.DataFrame({'Actual': y})['Actual'].plot(label='Actual', c='r', linestyle='--')
+    pyplot.plot(result)
+    pyplot.plot(y)
+    pyplot.show()
 
 
 def direction_value(x, y):
@@ -47,11 +58,16 @@ def group_by_stock(data, training_prop=.8, validation_prop=.1, test_prop=.1):
         except:
             group_by_dict[row[0]] = [row[1:]]
 
+    group_by_dict = {k: v for k, v in group_by_dict.items() if len(v) > 1600}
+
     data_size = len(min(group_by_dict.values(), key=len))
     train_size, val_size, test_size = int(data_size * training_prop), int(data_size * validation_prop), int(
         data_size * test_prop)
-    train_data = list(map(lambda x: np.array(group_by_dict[x])[-data_size: -data_size + train_size], group_by_dict.keys()))
-    val_data = list(map(lambda x: np.array(group_by_dict[x])[-data_size + train_size: -data_size + train_size + val_size], group_by_dict.keys()))
+    train_data = list(
+        map(lambda x: np.array(group_by_dict[x])[-data_size: -data_size + train_size], group_by_dict.keys()))
+    val_data = list(
+        map(lambda x: np.array(group_by_dict[x])[-data_size + train_size: -data_size + train_size + val_size],
+            group_by_dict.keys()))
     test_data = list(map(lambda x: np.array(group_by_dict[x])[-test_size:], group_by_dict.keys()))
     return np.array(train_data), np.array(val_data), np.array(test_data)
 
@@ -64,3 +80,34 @@ def get_feature_list_lags(features, lags=0):
             all_features = all_features + ['prev_' + feature + '_' + str(i)]
 
     return all_features
+
+
+# dict: { data1: [1,2,3], data2: [4,5,6]}
+def write_to_csv(filename, dict):
+    try:
+        with open(filename, 'w') as file:
+            csv.writer(file).writerows(list(map(lambda x: [x[0]] + x[1], dict.items())))
+    except:
+        print(filename)
+
+
+def from_args_to_filename(args):
+    test = json.dumps(args)
+    compressed = compress(test.encode('utf-8'))
+    return b64encode(compressed).decode('utf-8').replace('/', '$')
+
+
+def from_filename_to_args(filename):
+    decoded = b64decode(filename.replace('$', '/'))
+    return decompress(decoded)
+
+
+def plot_data(i, filepath):
+    with open(filepath) as file:
+        reader = csv.reader(file)
+        fig = plt.figure(i)
+        for row in reader:
+            plt.plot([float(i) for i in row[1:]], label=row[0])
+            plt.legend(loc='upper left')
+        fig.suptitle(f'{i}')
+        plt.show()
