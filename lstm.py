f0df7c4d0ead037534e462379904e9eb45e006f1
Jonas Laksen <jonaslaksen@live.com>
2019-05-23 13:19:46 +0200

    Feature search

--- lstm.py
+++ lstm.py
@@ -1,12 +1,9 @@
-import csv
-import json
-from os import listdir
-from random import sample, randint
+from copy import deepcopy
+from itertools import combinations
 
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from keras.callbacks import ModelCheckpoint
+from keras.callbacks import ModelCheckpoint, EarlyStopping
 from keras.losses import MAE
 from keras.optimizers import Adam
 from sklearn.preprocessing import MinMaxScaler
@@ -14,8 +11,7 @@ from sklearn.preprocessing import MinMaxScaler
 from models import bidir_lstm_seq
 from models.spec_network import SpecializedNetwork
 from models.stacked_lstm import StackedLSTM
-from utils import get_feature_list_lags, group_by_stock, write_to_csv, from_args_to_filename, \
-    from_filename_to_args, plot_data
+from utils import get_feature_list_lags, group_by_stock, print_metrics
 
 
 def load_data(feature_list):
@@ -54,11 +50,14 @@ def main(gen_epochs=0, spec_epochs=0, load_gen=True, load_spec=False, model_gene
     # Create the general model
     gen_model.compile(optimizer=Adam(learning_rate), loss=MAE)
     history = gen_model.fit([X_train] + zero_states, y_train, validation_data=([X_val] + zero_states, y_val),
-                            epochs=gen_epochs,
+                            epochs=gen_epochs * 1000,
+                            verbose=0,
                             shuffle=False,
-                            callbacks=[ModelCheckpoint('weights/gen.h5', period=10, save_weights_only=True), ])
+                            batch_size=batch_size,
+                            callbacks=[ModelCheckpoint('weights/gen.h5', period=10, save_weights_only=True),
+                                       EarlyStopping(monitor='val_loss', patience=20)])
 
-    write_to_csv(f'plot_data/gen/loss/{filename}.csv', history.history)
+    # write_to_csv(f'plot_data/gen/loss/{filename}.csv', history.history)
 
     gen_pred_model = model_generator(n_features=n_features, layer_sizes=layer_sizes, batch_size=batch_size,
                                      return_states=True, dropout=dropout)
@@ -79,7 +78,7 @@ def main(gen_epochs=0, spec_epochs=0, load_gen=True, load_spec=False, model_gene
     spec_model.fit([X_train] + stock_list, y_train, validation_data=([X_val] + stock_list, y_val),
                    batch_size=batch_size, epochs=spec_epochs, shuffle=False,
                    callbacks=[ModelCheckpoint('weights/spec.h5', period=1, save_weights_only=True)])
-    write_to_csv(f'plot_data/spec/loss/{filename}.csv', history.history)
+    # write_to_csv(f'plot_data/spec/loss/{filename}.csv', history.history)
     spec_pred_model = SpecializedNetwork(n_features=n_features, num_stocks=len(X_train), layer_sizes=layer_sizes,
                                          return_states=True, decoder=spec_model.decoder, is_bidir=is_bidir)
     spec_pred_model.set_weights(spec_model.get_weights())
@@ -98,69 +97,66 @@ def main(gen_epochs=0, spec_epochs=0, load_gen=True, load_spec=False, model_gene
         result_val, *new_states = model.predict([X_val] + new_states)
         result_test, *_ = model.predict([X_test] + new_states)
 
-        # Plot only inverse transformed results for one stock
-        stock_id = 10
         result_train, result_val, result_test, y_train_inv, y_val_inv, y_test_inv = map(
-            lambda x: scaler_y.inverse_transform(x[stock_id]).reshape(-1),
+            # lambda x: scaler_y.inverse_transform(x).reshape(-1),
+            lambda x: np.array(list(map(scaler_y.inverse_transform, x))),
             [result_train, result_val, result_test, y_train, y_val, y_test])
 
-        training = {f'training {"spec" if has_context else "gen"}': result_train.tolist(), 'y': y_train_inv.tolist()}
-        validation = {f'validation {"spec" if has_context else "gen"}': result_val.tolist(), 'y': y_val_inv.tolist()}
-        write_to_csv(f'plot_data/{"spec" if has_context else "gen"}/training/{filename}', training)
-        write_to_csv(f'plot_data/{"spec" if has_context else "gen"}/validation/{filename}', validation)
-    gen_pred_model.save(f'saved_models/gen/{filename}')
-    spec_pred_model.save(f'saved_models/spec/{filename}')
+        print_metrics(result_val, y_val_inv)
+        # training = {f'training {"spec" if has_context else "gen"}': result_train.tolist(), 'y': y_train_inv.tolist()}
+        # validation = {f'validation {"spec" if has_context else "gen"}': result_val.tolist(), 'y': y_val_inv.tolist()}
+        # write_to_csv(f'plot_data/{"spec" if has_context else "gen"}/training/{filename}', training)
+        # write_to_csv(f'plot_data/{"spec" if has_context else "gen"}/validation/{filename}', validation)
 
 
 # feature_list = ['positive', 'negative', 'neutral', 'open', 'high', 'low', 'volume', 'price']
 feature_list = ['volume', 'price']
 feature_list = get_feature_list_lags(feature_list, lags=2)
-feature_list = feature_list + ['open', 'high', 'low']
+feature_list = feature_list + ['positive', 'negative', 'neutral', 'positive_prop', 'negative_prop', 'neutral_prop',
+                               'trendscore', 'open', 'high', 'low']
+trading_features = [['price', 'volume'], ['open', 'high', 'low']]
+sentiment_features = [['positive', 'negative', 'neutral'], ['positive_prop', 'negative_prop', 'neutral_prop']]
+trendscore_features = [['trendscore']]
+s = trading_features + sentiment_features + trendscore_features
+temp = sum(map(lambda r: list(combinations(s, r)), range(1, len(s) + 1)), [])
+feature_subsets = list(map(lambda x: sum(x, []), temp))
 
 arguments = {
     'copy_weights_from_gen_to_spec': False,
     'feature_list': feature_list,
-    'dropout': .2,
-    'gen_epochs': 2,
+    'gen_epochs': 1,
     'spec_epochs': 0,
-    'layer_sizes': [41] * 1,
-    'learning_rate': .001,
     'load_gen': False,
     'load_spec': False,
     'model': 'stacked',
+    'dropout': .2,
+    'layer_sizes': [64],
+    'learning_rate': .01
     # 'model': 'bidir',
 }
 
+# Hyperparameter search
+# possible_hyperparameters = {
+#     'dropout': [0, .2, .5],
+#     'layer_sizes': [[8], [64], [256]],
+#     'learning_rate': [.001, .01, .1],
+# }
 
-def random_arguments():
-    hyperparameters = {
-        'feature_list': list(set(sample(feature_list, randint(1, len(feature_list))) + ['price'])),
-        'dropout': .1 * randint(1, 8),
-        'gen_epochs': 300,
-        'spec_epochs': 300,
-        'layer_sizes': [2 ** randint(1, 8)] * 1,
-        'learning_rate': 10 ** (-randint(1, 4)),
-        'model': 'stacked',
-        # 'model': 'bidir',
-    }
-    other_args = {
-        'copy_weights_from_gen_to_spec': False,
-        'load_gen': False,
-        'load_spec': False
-    }
-
-    return hyperparameters, other_args, from_args_to_filename(hyperparameters)
+# Feature search
+possible_hyperparameters = {
+    'feature_list': feature_subsets
+}
 
 
-# Generate random arguments to test an arbitrary amount of new models
-for i in range(100):
-    hyperparameters, other_args, filename = random_arguments()
-    print(hyperparameters)
-    main(**hyperparameters, **other_args,
-         model_generator=StackedLSTM if arguments['model'] == 'stacked' else bidir_lstm_seq.build_model,
-         filename=filename)
+def hyperparameter_search(possible, other_args):
+    for key in possible_hyperparameters.keys():
+        for possible_value in possible[key]:
+            args = deepcopy(other_args)
+            args[key] = possible_value
+            print({k: args[k] for k in possible_hyperparameters.keys() if k in args})
+            main(**args,
+                 model_generator=StackedLSTM if other_args['model'] == 'stacked' else bidir_lstm_seq.build_model,
+                 filename='test')
 
 
-all_files = list(map(lambda x: (json.loads(from_filename_to_args(x)), x), listdir('plot_data/spec/validation')))
-[plot_data(i, f'plot_data/spec/validation/{x}') for i, (_, x) in enumerate(all_files)]
-[print(i, json.loads(from_filename_to_args(x))) for i, (_, x) in enumerate(all_files)]
+hyperparameter_search(possible_hyperparameters, arguments)
