06cdeb66c632309abf0d77aedc3152d822ec4ebe
pthoang <pthoang94@gmail.com>
2019-05-27 13:37:52 +0200

    pull from master

--- lstm.py
+++ lstm.py
@@ -1,139 +1,162 @@
-from datetime import datetime
+from copy import deepcopy
+from itertools import combinations
 
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from keras.callbacks import ModelCheckpoint, TensorBoard
-from keras.losses import MAE, MAPE
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+from keras.losses import MAE
 from keras.optimizers import Adam
-from keras.utils import plot_model
-from sklearn import preprocessing
+from sklearn.preprocessing import MinMaxScaler
 
-import models.bidir_lstm_seq as bidir_lstm_seq
+from models import bidir_lstm_seq
 from models.spec_network import SpecializedNetwork
 from models.stacked_lstm import StackedLSTM
-from utils import get_feature_list_lags, group_by_stock, expand, plot
+from utils import get_feature_list_lags, group_by_stock, print_metrics
 
 
-def main(bidir_seq_epoch=0, gen_epochs=0, spec_epochs=0, load_gen=True, load_spec=False):
-    data = pd.read_csv('dataset.csv', index_col=0)
+def load_data(feature_list):
+    data = pd.read_csv('dataset_v2.csv', index_col=0)
     data = data.dropna()
-
-    # feature_list = ['positive', 'negative', 'neutral', 'open', 'high', 'low', 'volume', 'price']
-    feature_list = ['volume', 'price']
-    feature_list = get_feature_list_lags(feature_list, lags=2)
-    feature_list = feature_list + ['open', 'high', 'low']
-
-    n_features = len(feature_list)
-    layer_sizes = [41] * 3
-    batch_size = 17
-
-    scaler_X = preprocessing.MinMaxScaler()
-    scaler_y = preprocessing.MinMaxScaler()
+    scaler_X = MinMaxScaler()
+    scaler_y = MinMaxScaler()
     X = scaler_X.fit_transform(data[feature_list].values)
     y = scaler_y.fit_transform(data['next_price'].values.reshape(-1, 1))
     X = np.append(data['stock'].values.reshape(-1, 1), X, axis=1)
     y = np.append(data['stock'].values.reshape(-1, 1), y, axis=1)
 
-    X_no_pad, X, X_test = group_by_stock(X, n_features)
-    y_no_pad, y, y_test = group_by_stock(y, 1)
-    stock_id = 10
+    X_train, X_val, X_test = group_by_stock(X)
+    y_train, y_val, y_test = group_by_stock(y)
+    return (X_train, X_val, X_test), (y_train, y_val, y_test), scaler_y
 
-    X_train, X_test = expand(X_no_pad[stock_id]), expand(X_test[stock_id])
-    y_train = scaler_y.inverse_transform(y_no_pad[stock_id]).reshape(-1)
-    y_test = scaler_y.inverse_transform(y_test[stock_id]).reshape(-1)
 
-    gen_model = StackedLSTM(n_features=n_features, layer_sizes=layer_sizes, batch_size=batch_size, return_states=False)
+def main(gen_epochs=0, spec_epochs=0, load_gen=True, load_spec=False, model_generator=StackedLSTM, layer_sizes=[41],
+         copy_weights_from_gen_to_spec=False, feature_list=[], learning_rate=.001, dropout=.2, filename='test', **_):
+    (X_train, X_val, X_test), \
+    (y_train, y_val, y_test), \
+    scaler_y = load_data(feature_list)
+
+    n_features = X_train.shape[2]
+    batch_size = X_train.shape[0]
+    is_bidir = model_generator is not StackedLSTM
+    zero_states = [np.zeros((batch_size, layer_sizes[0]))] * len(layer_sizes) * 2 * (2 if is_bidir else 1)
+    stock_list = [np.arange(len(X_train)).reshape((len(X_train), 1, 1))]
+
+    gen_model = model_generator(n_features=n_features, layer_sizes=layer_sizes, batch_size=batch_size,
+                                return_states=False)
     if load_gen:
         gen_model.load_weights('weights/gen.h5')
         print('Loaded generalised model')
 
-    if gen_epochs > 0:
-        states = [np.zeros((batch_size, layer_sizes[0]))] * len(layer_sizes) * 2
-        gen_model.compile(optimizer=Adam(0.001), loss=MAE)
-        gen_model.fit([X] + states, y, batch_size=batch_size, epochs=gen_epochs, shuffle=False,
-                      callbacks=[
-                          # Save model every 10th epoch
-                          ModelCheckpoint('weights/gen.h5', period=10, save_weights_only=True),
-                          # Write to logfile to see graph in TensorBoard
-                          # TensorBoard(log_dir='logs/gen/{}'.format(datetime.now()))
-                      ])
+    # Create the general model
+    gen_model.compile(optimizer=Adam(learning_rate), loss=MAE)
+    history = gen_model.fit([X_train] + zero_states, y_train, validation_data=([X_val] + zero_states, y_val),
+                            epochs=gen_epochs * 1000,
+                            verbose=0,
+                            shuffle=False,
+                            batch_size=batch_size,
+                            callbacks=[ModelCheckpoint('weights/gen.h5', period=10, save_weights_only=True),
+                                       EarlyStopping(monitor='val_loss', patience=20)])
 
-        gen_pred_model = StackedLSTM(n_features=n_features, layer_sizes=layer_sizes, batch_size=1,
-                                     return_states=True)
-        gen_pred_model.set_weights(gen_model.get_weights())
+    # write_to_csv(f'plot_data/gen/loss/{filename}.csv', history.history)
 
-        states = [np.zeros((1, layer_sizes[0]))] * len(layer_sizes) * 2
-        result_train, *new_states = gen_pred_model.predict([X_train] + states)
-        result_train = scaler_y.inverse_transform(result_train[0]).reshape(-1)
+    gen_pred_model = model_generator(n_features=n_features, layer_sizes=layer_sizes, batch_size=batch_size,
+                                     return_states=True, dropout=dropout)
+    gen_pred_model.set_weights(gen_model.get_weights())
 
-        result_test, *_ = gen_pred_model.predict([X_test] + new_states)
-        result_test = scaler_y.inverse_transform(result_test[0]).reshape(-1)
+    # Create the context model, set the decoder = the gen model
+    decoder = model_generator(n_features=n_features, layer_sizes=layer_sizes, batch_size=batch_size, return_states=True,
+                              dropout=dropout)
+    if copy_weights_from_gen_to_spec:
+        decoder.set_weights(gen_model.get_weights())
+    spec_model = SpecializedNetwork(n_features=n_features, num_stocks=len(X_train), layer_sizes=layer_sizes,
+                                    decoder=decoder, is_bidir=is_bidir)
+    spec_model.compile(optimizer=Adam(learning_rate), loss=MAE)
+    if load_spec:
+        spec_model.load_weights('weights/spec.h5')
+        print('Loaded specialised model')
 
-        [plot(*x) for x in [(result_train, y_train), (result_test, y_test)]]
+    spec_model.fit([X_train] + stock_list, y_train, validation_data=([X_val] + stock_list, y_val),
+                   batch_size=batch_size, epochs=spec_epochs, shuffle=False,
+                   callbacks=[ModelCheckpoint('weights/spec.h5', period=1, save_weights_only=True)])
+    # write_to_csv(f'plot_data/spec/loss/{filename}.csv', history.history)
+    spec_pred_model = SpecializedNetwork(n_features=n_features, num_stocks=len(X_train), layer_sizes=layer_sizes,
+                                         return_states=True, decoder=spec_model.decoder, is_bidir=is_bidir)
+    spec_pred_model.set_weights(spec_model.get_weights())
 
-    if spec_epochs > 0:
-        spec_model = SpecializedNetwork(n_features=n_features, num_stocks=len(y), layer_sizes=layer_sizes,
-                                        batch_size=batch_size, init_all_layers=True)
-        spec_model.decoder.set_weights(gen_model.get_weights())
-        spec_model.compile(optimizer=Adam(0.001), loss=MAE)
-        if load_spec:
-            spec_model.load_weights('weights/spec.h5')
-            print('Loaded specialised model')
+    # The place for saving stuff for plotting
+    # Only plot if the epoch > 0
+    for model in ([gen_pred_model] if gen_epochs > 0 else []) + ([spec_pred_model] if spec_epochs > 0 else []):
+        has_context = isinstance(model, SpecializedNetwork)
+        # If general model, give zeros as input, if context give stock ids as input
+        init_state = model.encoder.predict(stock_list) if has_context else zero_states
 
-        [plot_model(x[1], 'model_plots/{}.png'.format(x[0]), show_shapes=True) for x in
-         [('gen', gen_model), ('spec', spec_model), ('encoder', spec_model.encoder)]]
+        if has_context:
+            model = model.decoder
 
-        X_stock_list = np.arange(len(X)).reshape((len(X), 1, 1))
-        spec_model.fit([X, X_stock_list], y, batch_size=batch_size, epochs=spec_epochs, shuffle=False,
-                       callbacks=[
-                           ModelCheckpoint('weights/spec.h5', period=1, save_weights_only=True),
-                           TensorBoard(log_dir='logs/spec/{}'.format(datetime.now()))])
+        result_train, *new_states = model.predict([X_train] + init_state)
+        result_val, *new_states = model.predict([X_val] + new_states)
+        result_test, *_ = model.predict([X_test] + new_states)
 
-        spec_pred_model = SpecializedNetwork(n_features=n_features, num_stocks=len(y), layer_sizes=layer_sizes,
-                                             batch_size=1, init_all_layers=True, return_states=True)
-        spec_pred_model.set_weights(spec_model.get_weights())
+        result_train, result_val, result_test, y_train_inv, y_val_inv, y_test_inv = map(
+            # lambda x: scaler_y.inverse_transform(x).reshape(-1),
+            lambda x: np.array(list(map(scaler_y.inverse_transform, x))),
+            [result_train, result_val, result_test, y_train, y_val, y_test])
 
-        result_train, *new_states = spec_pred_model.predict([X_train, np.array([[[stock_id]]])])
-        result_train = scaler_y.inverse_transform(result_train[0]).reshape(-1)
-
-        result_test, *_ = spec_pred_model.decoder.predict([X_test] + new_states)
-        result_test = scaler_y.inverse_transform(result_test[0]).reshape(-1)
-
-        print(result_train[-5:])
-        print(result_test[:5])
-
-        [plot(*x) for x in [(result_train, y_train), (result_test, y_test)]]
-
-    if bidir_seq_epoch > 0:
-        layer_size = 97
-        states = [np.zeros((batch_size, layer_size))] * 4
-        bidir_model = bidir_lstm_seq.build_model(n_features=n_features, layer_size=layer_size, stateful=False,
-                                                 batch_size=batch_size, return_states=False)
-
-        bidir_model.compile(optimizer=Adam(0.001), loss=MAE, metrics=[MAPE])
-
-        bidir_model.fit([X] + states, y, batch_size=batch_size, epochs=bidir_seq_epoch, shuffle=False,
-                        callbacks=[
-                            # Save model every 10th epoch
-                            # ModelCheckpoint('weights/bidir.h5', period=10, save_weights_only=True),
-                            # Write to logfile to see graph in TensorBoard
-                            # TensorBoard(log_dir='logs/gen/{}'.format(datetime.now()))
-                        ])
-
-        bidir_pred_model = bidir_lstm_seq.build_model(n_features=n_features, layer_size=layer_size, stateful=True,
-                                                      batch_size=1, return_states=True)
-        bidir_pred_model.set_weights(bidir_model.get_weights())
-
-        result_train, *new_states = bidir_pred_model.predict([X_train] + states)
-        result_train = scaler_y.inverse_transform(result_train[0]).reshape(-1)
-
-        result_test, *_ = bidir_pred_model.predict([X_test] + new_states)
-        result_test = scaler_y.inverse_transform(result_test[0]).reshape(-1)
-
-        [plot(*x) for x in [(result_train, y_train), (result_test, y_test)]]
-
-    plt.show()
+        print_metrics(result_val, y_val_inv)
+        # training = {f'training {"spec" if has_context else "gen"}': result_train.tolist(), 'y': y_train_inv.tolist()}
+        # validation = {f'validation {"spec" if has_context else "gen"}': result_val.tolist(), 'y': y_val_inv.tolist()}
+        # write_to_csv(f'plot_data/{"spec" if has_context else "gen"}/training/{filename}', training)
+        # write_to_csv(f'plot_data/{"spec" if has_context else "gen"}/validation/{filename}', validation)
 
 
-main(bidir_seq_epoch=250, gen_epochs=0, spec_epochs=0, load_gen=False, load_spec=False)
+# feature_list = ['positive', 'negative', 'neutral', 'open', 'high', 'low', 'volume', 'price']
+feature_list = ['volume', 'price']
+feature_list = get_feature_list_lags(feature_list, lags=2)
+feature_list = feature_list + ['positive', 'negative', 'neutral', 'positive_prop', 'negative_prop', 'neutral_prop',
+                               'trendscore', 'open', 'high', 'low']
+trading_features = [['price', 'volume'], ['open', 'high', 'low']]
+sentiment_features = [['positive', 'negative', 'neutral'], ['positive_prop', 'negative_prop', 'neutral_prop']]
+trendscore_features = [['trendscore']]
+s = trading_features + sentiment_features + trendscore_features
+temp = sum(map(lambda r: list(combinations(s, r)), range(1, len(s) + 1)), [])
+feature_subsets = list(map(lambda x: sum(x, []), temp))
+
+arguments = {
+    'copy_weights_from_gen_to_spec': False,
+    'feature_list': feature_list,
+    'gen_epochs': 1,
+    'spec_epochs': 0,
+    'load_gen': False,
+    'load_spec': False,
+    'model': 'stacked',
+    'dropout': .2,
+    'layer_sizes': [64],
+    'learning_rate': .01
+    # 'model': 'bidir',
+}
+
+# Hyperparameter search
+# possible_hyperparameters = {
+#     'dropout': [0, .2, .5],
+#     'layer_sizes': [[8], [64], [256]],
+#     'learning_rate': [.001, .01, .1],
+# }
+
+# Feature search
+possible_hyperparameters = {
+    'feature_list': feature_subsets
+}
+
+
+def hyperparameter_search(possible, other_args):
+    for key in possible_hyperparameters.keys():
+        for possible_value in possible[key]:
+            args = deepcopy(other_args)
+            args[key] = possible_value
+            print({k: args[k] for k in possible_hyperparameters.keys() if k in args})
+            main(**args,
+                 model_generator=StackedLSTM if other_args['model'] == 'stacked' else bidir_lstm_seq.build_model,
+                 filename='test')
+
+
+hyperparameter_search(possible_hyperparameters, arguments)
--- utils.py
+++ utils.py
@@ -1,7 +1,13 @@
 import copy
+import csv
+import json
+from base64 import b64encode, b64decode
+from zlib import compress, decompress
+import matplotlib.pyplot as plt
 
 import numpy as np
 import pandas as pd
+from matplotlib import pyplot
 from sklearn.metrics import mean_absolute_error, mean_squared_error, accuracy_score
 
 
@@ -12,15 +18,28 @@ def mean_absolute_percentage_error(y_true, y_pred):
     y_true, y_pred = np.array(y_true), np.array(y_pred)
     return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
 
-
-def plot(result, y):
+def print_metrics(result, y):
+    result = np.asarray(result).reshape((result.shape[0], -1))
+    y = np.asarray(y).reshape((result.shape[0], -1))
     mape = mean_absolute_percentage_error(y, result)
     mae = mean_absolute_error(y, result)
     mse = mean_squared_error(y, result)
-    accuracy_direction = direction_eval(result, y)
-    print(mape, mae, mse, accuracy_direction)
-    pd.DataFrame({'Predicted': result}).plot(label='Predicted', c='b')
-    pd.DataFrame({'Actual': y})['Actual'].plot(label='Actual', c='r', linestyle='--')
+    accuracy_direction = mean_direction_eval(result, y)
+    print({'MAPE': mape, 'MAE': mae, 'MSE': mse, 'DA': accuracy_direction})
+
+def plot(title, result, y):
+    # result = np.asarray(result)
+    # y = np.asarray(y)
+    # mape = mean_absolute_percentage_error(y, result)
+    # mae = mean_absolute_error(y, result)
+    # mse = mean_squared_error(y, result)
+    # accuracy_direction = direction_eval(result, y)
+    # print(mape, mae, mse, accuracy_direction)
+    # pd.DataFrame({'Predicted': result}).plot(label='Predicted', c='b', title=title)
+    # pd.DataFrame({'Actual': y})['Actual'].plot(label='Actual', c='r', linestyle='--')
+    pyplot.plot(result)
+    pyplot.plot(y)
+    pyplot.show()
 
 
 def direction_value(x, y):
@@ -31,6 +50,8 @@ def direction_value(x, y):
     else:
         return 0
 
+def mean_direction_eval(result, y):
+    return np.mean(np.array(list(map(lambda x: direction_eval(x[0], x[1]), zip(result, y)))))
 
 def direction_eval(result, y):
     result_pair = list(map(lambda x, y: direction_value(x, y), result[:-1], result[1:]))
@@ -38,7 +59,8 @@ def direction_eval(result, y):
     return accuracy_score(y_pair, result_pair)
 
 
-def group_by_stock(data, n_features):
+def group_by_stock(data, training_prop=.8, validation_prop=.1, test_prop=.1):
+    assert training_prop + validation_prop + test_prop == 1.
     group_by_dict = {}
     for row in data:
         try:
@@ -46,16 +68,18 @@ def group_by_stock(data, n_features):
         except:
             group_by_dict[row[0]] = [row[1:]]
 
-    data = list(map(lambda x: np.array(group_by_dict[x])[:-50], group_by_dict.keys()))
-    test_data = list(map(lambda x: np.array(group_by_dict[x])[-50:], group_by_dict.keys()))
+    group_by_dict = {k: v for k, v in group_by_dict.items() if len(v) > 1600}
 
-    data_padded = copy.deepcopy(data)
-
-    max_length = np.max([x.shape[0] for x in data])
-    for i in range(len(data)):
-        data_padded[i] = np.append(data_padded[i], np.zeros((max_length - data_padded[i].shape[0], n_features)), axis=0)
-
-    return np.array(data), np.array(data_padded), np.array(test_data)
+    data_size = len(min(group_by_dict.values(), key=len))
+    train_size, val_size, test_size = int(data_size * training_prop), int(data_size * validation_prop), int(
+        data_size * test_prop)
+    train_data = list(
+        map(lambda x: np.array(group_by_dict[x])[-data_size: -data_size + train_size], group_by_dict.keys()))
+    val_data = list(
+        map(lambda x: np.array(group_by_dict[x])[-data_size + train_size: -data_size + train_size + val_size],
+            group_by_dict.keys()))
+    test_data = list(map(lambda x: np.array(group_by_dict[x])[-test_size:], group_by_dict.keys()))
+    return np.array(train_data), np.array(val_data), np.array(test_data)
 
 
 def get_feature_list_lags(features, lags=0):
@@ -66,3 +90,34 @@ def get_feature_list_lags(features, lags=0):
             all_features = all_features + ['prev_' + feature + '_' + str(i)]
 
     return all_features
+
+
+# dict: { data1: [1,2,3], data2: [4,5,6]}
+def write_to_csv(filename, dict):
+    try:
+        with open(filename, 'w') as file:
+            csv.writer(file).writerows(list(map(lambda x: [x[0]] + x[1], dict.items())))
+    except:
+        print(filename)
+
+
+def from_args_to_filename(args):
+    test = json.dumps(args)
+    compressed = compress(test.encode('utf-8'), 9)
+    return b64encode(compressed).decode('utf-8').replace('/', '$')
+
+
+def from_filename_to_args(filename):
+    decoded = b64decode(filename.replace('$', '/').split('.csv')[0])
+    return decompress(decoded)
+
+
+def plot_data(i, filepath):
+    with open(filepath) as file:
+        reader = csv.reader(file)
+        fig = plt.figure(i)
+        for row in reader:
+            plt.plot([float(i) for i in row[1:]], label=row[0])
+            plt.legend(loc='upper left')
+        fig.suptitle(f'{i}')
+        plt.show()
