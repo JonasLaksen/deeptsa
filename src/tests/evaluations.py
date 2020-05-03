86bbb0d9c3483a77d613ba30f1e3d273b2c03786
Jonas Laskemoen <jonasl@netcompany.com>
2020-05-03 12:24:20 +0200

    Fix Direction Accuracy

--- src/tests/evaluations.py
+++ src/tests/evaluations.py
@@ -30,14 +30,19 @@ class TestEvaluations(unittest.TestCase):
     def test_da_price(self):
         results = np.asarray([[[1], [ 1 ], [ 1 ], [ 1 ], [ 1 ], [ 1 ], [ 1 ], [ -2 ], [ -1 ]]])
         y = np.asarray([[[ 1 ], [ 1 ], [ 1 ], [ -1 ], [ 1 ], [ 1 ], [ 1 ], [ -1 ], [ -2 ]]])
-        DA = evaluate(results, y, y_type='price')['DA']
+        DA = evaluate(results, y, y_type='next_price')['DA']
         print(DA.round(3))
         assert DA.round(3) == .750
 
         r2 = np.asarray([1, 2, 3 ]).reshape((1,1,3))
         y2 = np.asarray([1, 2, -1 ]).reshape((1,1,3))
-        DA2 = evaluate(r2, y2, y_type='price')['DA']
+        DA2 = evaluate(r2, y2, y_type='next_price')['DA']
         assert DA2.round(3) ==  0.5
+
+        y3 = np.asarray([5, 2, 1 ]).reshape((1,1,3))
+        r3 = np.asarray([5, 6, 5 ]).reshape((1,1,3))
+        DA3 = evaluate(r3, y3, y_type='next_price')['DA']
+        assert DA3.round(3) ==  0
 
 if __name__ == '__main__':
     unittest.main()
